75a815667e66abb8b08e3767390cbc3d8683b86f
Harrygill-cyber <revolutiongill@gmail.com>
2024-05-31 08:36:10 +0530

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Harry Gill on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
